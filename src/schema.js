26aeab53e6098976c8eb5b9505193a71b54aa861
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-08-16 21:58:04 -0700

    fix(binding): generated

---
+++ src/schema.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+const { HttpLink } = require("apollo-link-http");
+const { makeRemoteExecutableSchema } = require("graphql-tools");
+
+const typeDefs = `
+  type Query {
+    user(id: ID!): User
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): User!
+    updateUser(id: ID!, name: String!): User
+    deleteUser(id: ID!): User
+  }
+
+  type Subscription {
+    userCreated: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+  }
+`;
+
+// Create the `HttpLink` required for the remote executable schema
+const endpoint = `https://graphql-binding-example-service-kcbreqbsbh.now.sh`;
+const link = new HttpLink({ uri: endpoint, fetch });
+
+// Create the remote schema
+module.exports = { schema: makeRemoteExecutableSchema({ link, schema: typeDefs }) };
